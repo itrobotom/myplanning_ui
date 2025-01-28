d7fc25d8a721da5c5f50b5317cc073dc9bce5e48
itrobotom <russk1j@yandex.ru>
2025-01-28 18:53:15 +0700

    Логика работы таски, раскрытие содержимого

--- src/components/CardTask/CardTask.jsx
+++ src/components/CardTask/CardTask.jsx
@@ -28,19 +28,37 @@ import Filter3Icon from '@mui/icons-material/Filter3';
 
 function CardTask({props}) {
     //если развернутая карточка задачи, то она выше
-    const heightCard = 200;
+    //const heightCard = 200;
     //вытащим данные из стора по isCheckBoxTaskStatus, таким и будет далее стейт
     //а пока сделаем его просто по дефолту false
-    const isLearn = true; //режим обучения (включается возможность кликать по всем иконкам, получая подсказки)
-    const isFullTask = false; //все задачи в ленте свернуты, чтобы развернуть и увидеть все настройки и все содержимое, возомжность редактироват, надо нажать на саму задачу
-    const isCheckBoxTaskStatusDefault = false; 
-    const [statusTask, setStatusTask] = useState(isCheckBoxTaskStatusDefault);
+    const FULL_HEIGHT = 200;
+    const SHORT_HEIGHT = 200;
+    //const isLearnMode = true; 
+    //const isFullTask = false; //все задачи в ленте свернуты, чтобы развернуть и увидеть все настройки и все содержимое, возомжность редактироват, надо нажать на саму задачу
+    // const isCheckBoxTaskStatusDefault = false; 
+    const [heightCard, setHeightCard] = useState(SHORT_HEIGHT); 
+    const [statusTask, setStatusTask] = useState(false);
+    const [isOpenTask, setIsOpenTask] = useState(true); 
+    const [isEditTask, setIsEditTask] = useState(true);
+    const [isLearnMode, setIsLearnMode] = useState(true); //режим обучения (включается возможность кликать по всем иконкам, получая подсказки)
+    const [isRepetTask, setIsRepeatTask] = useState(true); 
+    const [repeatDaysOfWeak, setRepeatDaysOfWeak] = useState(false); //при повторе по дням недели значек переносится вниз и указывадются дни недели рядом для повтора 
     const handleTaskStatus = () => { //при клике меняем статус
         setStatusTask(!statusTask);
         console.log(`Статус таски: ${statusTask}`);
     }
+    const handleOpenTask = () => {
+        setIsOpenTask(!isOpenTask);
+        isOpenTask ? setHeightCard(FULL_HEIGHT) : setHeightCard(SHORT_HEIGHT);
+        console.log(`Задача развернута ${isOpenTask}`); 
+    }
+    const handleEditTask = () => {
+        setIsEditTask(!isEditTask);
+        console.log(`Задача находится в режиме редактирования ${isEditTask}`)
+    }
     return (
-        <Card className="main-container-task" sx={{ height: heightCard }}>
+        // <Card className="main-container-task" sx={{ height: heightCard }}>
+        <Card>
             {/* <CardContent>
                 <Typography variant="h5">Заголовок</Typography>
                 <Typography>Контент карточки</Typography>
@@ -57,78 +75,103 @@ function CardTask({props}) {
                     />
                 </Box>
                 <Box className="middle-task-block">
-                    <Box className="middle-task-block-text">
-                        {/* <Typography color="#000000" variant="h6">Установить программное обеспечение на ПК К20-3. Настроить программы, подключить проектор к пк и веб камеру</Typography> */}
+                    {/* меняем класс при разворачивании задачи */}
+                    <Box 
+                        className={`${isOpenTask ? 'middle-task-block.expanded-text' : 'middle-task-block-text'}`}
+                        onClick={handleOpenTask}
+                    >
                         <Typography>Установить программное обеспечение на ПК К20-3. Настроить программы, подключить проектор к пк и веб камеру</Typography>
+                        
                     </Box>
                     {/* иконки и настройки ниже самой задачи */}
                     <Box sx={{display: "flex", mr:"10px"}}>
-                        <IconButton>
-                            <DeleteOutlineIcon></DeleteOutlineIcon>
-                        </IconButton>
-                        <IconButton>
-                            <NotificationsNoneIcon></NotificationsNoneIcon>
-                        </IconButton>
-                        
-                        {isFullTask && 
-                            <IconButton>
-                                <Box
-                                    sx={{
-                                        position: 'relative',
-                                        display: 'inline-flex',
-                                        alignItems: 'center',
-                                        justifyContent: 'center',
-                                    }}
-                                >
-                                    {/* Сам значок Repeat */}
-                                    <RepeatIcon />
-                                    {/* Линия перечеркивания */}
-                                    <Box className="crossing-out"/>
+                        {isOpenTask &&
+                            <Box sx={{
+                                display: repeatDaysOfWeak ? "block" : "flex",
+                            }}>
+                                <Box>
+                                    <IconButton>
+                                        <DeleteOutlineIcon></DeleteOutlineIcon>
+                                    </IconButton>
+                                    <IconButton>
+                                        <NotificationsNoneIcon></NotificationsNoneIcon>
+                                    </IconButton>
                                 </Box>
-                            </IconButton>
+
+                                <IconButton>
+                                    <Box
+                                        sx={{
+                                            position: 'relative',
+                                            display: 'inline-flex',
+                                            alignItems: 'center',
+                                            justifyContent: 'center',
+                                        }}
+                                    >
+                                        {/* Сам значок Repeat */}
+                                        <RepeatIcon />
+                                        {/* Линия перечеркивания */}
+                                        <Box className="crossing-out"/>
+                                    </Box>
+                                </IconButton>
+                            </Box>
                         }
                     </Box>
+                    
+                    
                 </Box>
                 
                 <Box className="right-task-block" sx={{border: "1px solid black"}}>
-                    {/* две иконки рядом (повтор и статус) */}
-                    <Box sx={{display: "flex", justifyContent: "space-between"}}>
-                        <IconButton>
-                            <Box
-                                sx={{
-                                    position: 'relative',
-                                    display: 'inline-flex',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                }}
+                    {/* две иконки рядом друг под другом(повтор и статус) */}
+                    {/* <Box sx={{display: "flex", justifyContent: "space-between"}}> */}
+                    <Box sx={{display: "flex", flexDirection: "column", justifyContent: "space-between", height: "100%"}}>
+                        <Box>
+                            {isLearnMode ? (
+                                <IconButton className={isLearnMode ? 'rotatePause' : 'noAnimation'}>
+                                    <Filter1Icon></Filter1Icon>
+                                </IconButton>
+                            ) : (
+                                <Box className='icon-no-button'>
+                                    <Filter1Icon />
+                                </Box>
+                            )}
+
+                            {!isOpenTask && 
+                                <IconButton>
+                                    <Box
+                                        sx={{
+                                            position: 'relative',
+                                            display: 'inline-flex',
+                                            alignItems: 'center',
+                                            justifyContent: 'center',
+                                        }}
+                                    >
+                                        {/* Сам значок Repeat */}
+                                        <RepeatIcon />
+
+                                        {/* Линия перечеркивания */}
+                                        <Box className="crossing-out"/>
+                                    </Box>
+                                </IconButton>
+                            }
+                        </Box>
+                        {/* иконка редактирования, которая появляется внизу после разворачивания карточки задачи */}
+                        {isEditTask ? 
+                        // сделать иконку сохранения с анимацией, чтобы было видно, что измнения надо сохранть
+                        //надо иконкой сохранения можно сделать иконку выхода из режима редактирования - крестик
+                            (<IconButton>
+                                <SaveIcon></SaveIcon>
+                            </IconButton>) : (
+                            
+                            <IconButton
+                                onClick={handleEditTask}
                             >
-                                {/* Сам значок Repeat */}
-                                <RepeatIcon />
-
-                                {/* Линия перечеркивания */}
-                                <Box className="crossing-out"/>
-                            </Box>
-                        </IconButton>
-
-         
-                        {isLearn ? (
-                            <IconButton className={isLearn ? 'rotatePause' : 'noAnimation'}>
-                                <Filter1Icon></Filter1Icon>
+                                <EditIcon></EditIcon>
                             </IconButton>
-                        ) : (
-                            <Box className='icon-no-button'>
-                                <Filter1Icon />
-                            </Box>
                         )}
 
                     </Box>
-                    {/* иконка редактирования, которая появляется внизу после разворачивания карточки задачи */}
-                    <IconButton>
-                        <EditIcon></EditIcon>
-                    </IconButton>
-                    {/* <IconButton>
-                        <SaveIcon></SaveIcon>
-                    </IconButton> */}
+
+                    
                 </Box>
             </Box>
 

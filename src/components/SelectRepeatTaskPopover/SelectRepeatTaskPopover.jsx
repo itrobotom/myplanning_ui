8ea5ff580685da67322e864d69c238805f208102
itrobotom <russk1j@yandex.ru>
2025-03-04 19:02:23 +0700

    Начал редюсер для повторов задачи

--- src/components/SelectRepeatTaskPopover/SelectRepeatTaskPopover.jsx
+++ src/components/SelectRepeatTaskPopover/SelectRepeatTaskPopover.jsx
@@ -10,17 +10,50 @@ import Switch from '@material-ui/core/Switch';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import { Button, Popover, Typography, IconButton } from "@mui/material";
-import { useState } from 'react';
+import { useState, useReducer } from 'react';
+
+
+//редьюсер для смены настроек повторов
+
+function reducerRepeatTask(repeatSettings, action) {
+    switch(action.type) {
+        case TOGGLE_REPEAT:
+            if(repeatStatus) {
+                return ({...repeatSettings,
+                    repeatStatus: !repeatStatus,
+                    repeatMode: everyday, //дефолтно ставим повтор каждый день
+                });
+            }
+            else {
+                return ({...repeatSettings,
+                    repeatStatus: !repeatStatus,
+                    repeatMode: null,
+                });
+            }
+            
+        case REPEAT_MODE:
+            return ({...repeatSettings,
+                //из payload Надо достать именно режим, т.к. в нем еще могут лежать и дни недели
+                repeatMode: action.payload //загружаем режим повторов
+            }) 
+    }
+}
+
 
 function SelectRepeatTaskPopover ({isLearnMode, isEditTask, repeatSettignsObj}) {
     // const [repeatStatus, setRepeatStatus] = useState(false);
+
     const initRepeatSettings = {
         repeatStatus: false,
         repeatMode: null, //everyday, everyWeek, everyMonth, everyYear, choiceDays
         choiceDaysRepeat: null //["sun", "mon", "tu", "we", "th", "fr", "sat"]
     }
 
-    const repeatSettings = {
+    const [repeatSettings, dispatch] = useReducer(reducerRepeatTask, initRepeatSettings);
+    
+    
+
+    const repeatSettingssss = {
         repeatStatus: true,
         repeatMode: "everyday", //everyday, everyWeek, everyMonth, everyYear, choiceDays
         choiceDaysRepeat: ["tu", "fr"] //["sun", "mon", "tu", "we", "th", "fr", "sat"]
